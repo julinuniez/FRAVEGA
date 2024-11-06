32ac3e173f007321b5ca667c815745e487705070
Julian Nuñez <JulianLeonel.Nunez@Alumnos.uai.edu.ar>
2024-11-06 19:27:22 -0300

    Mapper Venta y Detalle Venta

---
+++ DAL/MP_DetalleVenta.cs
@@ -0,0 +1,49 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class MP_DetalleVenta
+    {
+        Acceso acceso = new Acceso();
+        public int AgregarDetalleVenta(DetalleVenta detalleVenta)
+        {
+            SqlParameter[] parametros = new SqlParameter[]
+            {
+            new SqlParameter("@nroVenta", detalleVenta.nroVenta),
+            new SqlParameter("@idProducto", detalleVenta.idProducto),
+            new SqlParameter("@cantidad", detalleVenta.Cantidad),
+            new SqlParameter("@precio", detalleVenta.Subtotal)
+            };
+            return acceso.escribir("AgregarDetalleVenta", parametros);
+        }
+
+        public List<DetalleVenta> ListarDetalleVenta(int nroVenta)
+        {
+            List<DetalleVenta> lista = new List<DetalleVenta>();
+            SqlParameter[] parametros = new SqlParameter[]
+            {
+            new SqlParameter("@nroVenta", nroVenta)
+            };
+            DataTable dt = acceso.leer("ListarDetalleVenta", parametros);
+            foreach (DataRow dr in dt.Rows)
+            {
+                DetalleVenta detalleVenta = new DetalleVenta();
+                detalleVenta.idDetalleVenta = Convert.ToInt32(dr["idDetalleVenta"]);
+                detalleVenta.nroVenta = Convert.ToInt32(dr["nroVenta"]);
+                detalleVenta.idProducto = Convert.ToInt32(dr["idProducto"]);
+                detalleVenta.Cantidad = Convert.ToInt32(dr["cantidad"]);
+                detalleVenta.Subtotal = Convert.ToDecimal(dr["precio"]);
+                lista.Add(detalleVenta);
+            }
+            return lista;
+        }
+
+    }
+}
---
+++ DAL/MP_Venta.cs
@@ -0,0 +1,46 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class MP_Venta
+    {
+        Acceso acceso = new Acceso();
+
+        public int AgregarVenta(Venta venta)
+        {
+            SqlParameter[] parametros = new SqlParameter[]
+            {
+            new SqlParameter("@idCliente", venta.idCliente),
+            new SqlParameter("@Total", venta.total),
+            new SqlParameter("@Fecha", venta.fecha),
+            new SqlParameter("@NroTarjeta", venta.nroTarjeta)
+            };
+            return acceso.escribir("AgregarVenta", parametros);
+        }
+
+        public List<Venta> ListarVentas()
+        {
+            List<Venta> lista = new List<Venta>();
+            DataTable dt = acceso.leer("ListarVentas", null);
+            foreach (DataRow dr in dt.Rows)
+            {
+                Venta venta = new Venta();
+                venta.nroVenta = Convert.ToInt32(dr["nroVenta"]);
+                venta.idCliente = Convert.ToInt32(dr["idCliente"]);
+                venta.total = Convert.ToDecimal(dr["total"]);
+                venta.fecha = Convert.ToDateTime(dr["fecha"]);
+                venta.nroTarjeta = Convert.ToInt32(dr["nroTarjeta"]);
+                lista.Add(venta);
+            }
+            return lista;
+        }
+
+    }
+}
