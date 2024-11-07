231846e85d49dce3c36dea5815a68c27f139403a
Julian Nuñez <JulianLeonel.Nunez@Alumnos.uai.edu.ar>
2024-11-06 21:23:58 -0300

    BLL Detalle Venta y Venta

--- BLL/BLLDetalleVenta.cs
+++ BLL/BLLDetalleVenta.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using BE;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 
 namespace BLL
 {
-    internal class BLLDetalleVenta
+    public class BLLDetalleVenta
     {
+        MP_DetalleVenta mapper = new MP_DetalleVenta();
+
+        public int AgregarDetalleVenta(DetalleVenta detalleVenta)
+        {
+            return mapper.AgregarDetalleVenta(detalleVenta);
+        }
+
+        public List<DetalleVenta> ListarDetalleVenta(int nroVenta)
+        {
+            return mapper.ListarDetalleVenta(nroVenta);
+        }
     }
 }
--- BLL/BLLVenta.cs
+++ BLL/BLLVenta.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
+using BE;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace BLL
 {
-    internal class BLLVenta
+    public class BLLVenta
     {
+        MP_Venta mapper = new MP_Venta();
+
+        public int AgregarVenta(Venta venta)
+        {
+            return mapper.AgregarVenta(venta);
+        }
+
+        public List<Venta> ListarVentas()
+        {
+            return mapper.ListarVentas();
+        }
     }
 }
